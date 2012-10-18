e231c9e2bd9a139dab36f2a9c644548504822816
MrDHat <ktyaks@gmail.com>
2012-10-18 17:18:17 +0530

    Added Basic Parser Framework

---
+++ parser.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdio.h>
+#include<cstring>
+#include<queue>
+
+
+using namespace std ;
+
+class parser
+{
+private: 
+	
+	
+public:
+	
+	parser()
+	{
+		  
+	}
+	
+	int parseLineFromQueue(queue<string> inputLine)					//Replace words in queue
+	{
+		  
+	}
+	
+};
+
+int main()
+{
+	FILE *in, *out ;					//File Pointers
+	
+	in = fopen("Input.c" , "r") ;
+	out = fopen("Output.c", "w+") ;				//File Name to be REVISED!
+	
+	
+	
+	return 0;
+	
+}
