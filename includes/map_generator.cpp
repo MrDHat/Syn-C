05776ba6d02304cbd9725fa27c4db5bae1a9c69a
MrDHat <ktyaks@gmail.com>
2013-03-07 18:54:15 +0530

    Architectural Foundation

---
+++ includes/map_generator.cpp
@@ -0,0 +1,16 @@
+#include "map_generator.h"
+#include <string>
+
+const string xml_path("../XML/");
+
+parser::parser() {
+
+}
+
+int parser::xml_reader() {
+
+}
+
+int parser::create_function_map(string key, string value) {
+
+}
---
+++ includes/map_generator.h
@@ -0,0 +1,38 @@
+/*
+Parses the XML file, generates the unordered map
+*/
+
+#ifndef map_generator_h__
+#define map_generator_h__
+
+#include <unordered_map>
+#include <string>
+#include "PugiXML/pugixml.cpp"
+
+using namespace std;
+using namespace pugi;
+
+typedef unordered_map<string,string> string_map;
+
+class parser {
+	private:
+		
+		string_map function_map;	// Data Structure to store the hash map
+
+		//Pugi XML variables
+		xml_document xml_header_file;
+		xml_parse_result xml_result;
+		xml_node xml_tool;
+		xml_node_iterator xml_iterator;
+		xml_attribute xml_attrib;
+		xml_attribute_iterator xml_a_iterator;
+	
+	public:
+
+		parser();
+		int create_function_map(string, string);
+		int xml_reader();
+
+};
+
+#endif
